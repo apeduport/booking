3f7e99d1cd5044905ac65720b13a4f242c6c30b9
Marblism Editor <editor@marblism.com>
2025-01-28 03:39:50 +0000

    change root routing

---
+++ app/routes/index.tsx
@@ -0,0 +1,12 @@
+import { useNavigate } from '@remix-run/react'
+import { useEffect } from 'react'
+
+export default function Index() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    navigate('/home')
+  }, [])
+
+  return null
+}
